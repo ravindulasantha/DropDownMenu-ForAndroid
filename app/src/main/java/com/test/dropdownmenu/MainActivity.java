4da18f2b5d4618c67edb593f77acf81edabe062a
Ravindu Lasantha <67038119+ravindulasantha@users.noreply.github.com>
2023-05-09 08:05:42 +0530

    dropdown menu

---
+++ app/src/main/java/com/test/dropdownmenu/MainActivity.java
@@ -0,0 +1,36 @@
+package com.test.dropdownmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] items =  {"Material","Design","Components","Android","5.0 Lollipop"};
+    AutoCompleteTextView autoCompleteTxt;
+    ArrayAdapter<String> adapterItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        autoCompleteTxt = findViewById(R.id.auto_complete_txt);
+
+        adapterItems = new ArrayAdapter<String>(this,R.layout.list_item,items);
+        autoCompleteTxt.setAdapter(adapterItems);
+
+        autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(),"Item: "+item, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
